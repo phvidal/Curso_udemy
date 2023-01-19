6d19a5f832e8bee2e6d10426e98b6f10e3f792f4
phvidal <vidal081089@gmail.com>
2023-01-18 22:52:27 -0300

    Inclusão dos operadores aritméticos

--- Secao03/sec03_class24_var.py
+++ Secao03/sec03_class24_var.py
@@ -1,3 +1,22 @@
+# Variáveis são uma forma de associar a um nome um valor, basicamente.
+# Esse nome receberá o valor e será chamado de variável
+# Podemos atribuir outra variável a uma variável nova, e essa recebera o valor da antiga.
+
+idade = 25
+print(idade)
+minha_idade = idade
+
+print(minha_idade)
+
+# Cálculos com variáveis 
+
+base = 5
+altura = 8
+area = (base * altura) / 2
+print(area)
+
+
+# Exemplos de usos de variáveis imprimindo os tipos de dados que vimos até o momento
 
 nome = 'Paulo'
 sobrenome = 'Vidal'
@@ -12,3 +31,12 @@ print('Idade:', idade)
 print('Ano de nascimento:', ano_nascimento)
 print('É maior de idade?:', maior_de_idade)
 print('Altura em metros:', altura_metros)
+
+# Existem regras para criação de variáveis, por convenção elas sempre começam com a letra minúscula
+# Podem conter caracteres alfanuméricos (a até z), (A até Z) e (0 até 9).
+# Podem conter alguns caracteres especiais, como o underline.
+#mas não podem conter algumas palavras, são elas:
+
+""" and, as, assert, break, class, continue, def, del, elif, else, except,
+exec, finally, for, from, global, if, import, in, is, lambda, not, or,
+pass, print, raise, return, try, while, with, yield """
---
+++ Secao03/sec03_class26_int_operadores_aritmeticos.py
@@ -0,0 +1,79 @@
+'''Vamos falar sobre Operadores aritméticos em Python'''
+
+
+# ADIÇÃO
+
+adicao = 10 + 10
+print('Adição',type(adicao), adicao)
+
+adicao_02 = 10 + 10.5
+print('Adição',type(adicao_02), adicao_02)
+
+adicao_03 = 10.5 + 10.5
+print('Adição',type(adicao_03), adicao_03)
+
+# SUBTRAÇÃO
+
+subtracao = 10 - 5
+print('Subtração', type(subtracao), subtracao)
+
+subtracao_02 = 10 - 5.5
+print('Subtração', type(subtracao_02), subtracao_02)
+
+subtracao_03 = 10.5 - 5.5
+print('Subtração', type(subtracao_03), subtracao_03)
+
+# MULTIPLICAÇÃO
+
+multiplicacao = 10 * 10
+print('Multiplicação', type(multiplicacao), multiplicacao)
+
+multiplicacao_02 = 10 * 10.5
+print('Multiplicação', type(multiplicacao_02), multiplicacao_02)
+
+multiplicacao_03 = 10.5 * 10.3
+print('Multiplicação', type(multiplicacao_03), multiplicacao_03)
+
+# DIVISÃO
+
+divisao = 10 / 2
+print('Divisão', type(divisao), divisao)
+
+divisao_02 = 10 / 3
+print('Divisão', type(divisao_02), divisao_02)
+
+divisao_03 = 10 / 3.5
+print('Divisão', type(divisao_03), divisao_03)
+
+# DIVISÃO INTEIRA
+
+divisao_inteira = 10 // 2
+print('Divisão Inteira', type(divisao_inteira), divisao_inteira)
+
+divisao_inteira_02 = 10 // 3
+print('Divisão Inteira', type(divisao_inteira_02), divisao_inteira_02)
+
+divisao_inteira_03 = 10 // 3.5
+print('Divisão Inteira', type(divisao_inteira_03), divisao_inteira_03)
+
+
+# EXPONENCIAÇÃO
+
+exponencicacao = 2 ** 10
+print('Exponenciação', type(exponencicacao), exponencicacao)
+
+exponencicacao_02 = 2 ** 10.5
+print('Exponenciação', type(exponencicacao_02), exponencicacao_02)
+
+
+# MÓDULO 
+
+modulo = 55 % 2.7
+print('Módulo', type(modulo), modulo) 
+
+modulo_02 = 50 % 2
+print('Módulo', type(modulo_02), modulo_02)
+
+modulo_03 = 55 % 2
+print('Módulo', type(modulo_03), modulo_03)
+
