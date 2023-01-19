804f8d4d0ba051995aae1591ffbb32c341bc7125
phvidal <vidal081089@gmail.com>
2023-01-19 14:02:23 -0300

    Incluindo aula 27, repetições e concatenações

---
+++ Secao03/secao03_class27_conc_rep.py
@@ -0,0 +1,18 @@
+# Vamos ver os operadores de adição para efetuar Concatenações
+# E o operador de multiplicação para efetuar Repetições
+
+# CONCATENANDO
+
+concatenando = 'Paulo' + 'Vidal'
+print(concatenando)
+
+concatenando_02 = 'Paulo' + ' ' + 'Vidal'
+print(concatenando_02)
+
+""" concatenando_03 = 'Paulo' + 2 + 'Vidal' # vai dar erro pois não pode somar INT com STR
+print(concatenando_03) """
+
+# REPETINDO
+
+repetindo = 5 * 'Vidal '
+print(repetindo)
