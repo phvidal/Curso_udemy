4ed6745c73611df1e8b1d613de568d6f0ef11286
phvidal <vidal081089@gmail.com>
2023-01-23 23:35:27 -0300

    IF / ELIF / ELSE

---
+++ Secao03/sec03_class34_if_elif_else.py
@@ -0,0 +1,10 @@
+# Ferramentas de controle de fluxo
+
+acesso = input('VocÊ deseja acessar o sistema? Sim ou Não? ')
+
+if acesso == 'Sim':
+    print('Bem vindo ao sistema!')
+elif acesso == 'Não':
+    print('Você saiu do sistema.')
+else:
+    print('Por favor, escolha "Sim" ou "Não"')
