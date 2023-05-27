7369fda8117cb8e5860d59fa247b05c468cef22b
phvidal <vidal081089@gmail.com>
2023-05-27 13:23:01 -0300

    trabalhando com listas

--- aulas_extras/foor_loop.py
+++ aulas_extras/foor_loop.py
@@ -1,3 +1,6 @@
 magicians = ['alice', 'david', 'carolina']
 for magican in magicians:
-    print(magican)
+    print(f'Congrats {magican.title()} that was a great trick!')
+    print(f"I can't wait to see your next trick, {magican.title()} \n")
+
+    print("Thank you, everyone, that was a great magic show!!!!")
