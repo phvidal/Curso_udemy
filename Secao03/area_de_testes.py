5e3174a1857f1ab5a1744f28da390b67c0242ff8
phvidal <vidal081089@gmail.com>
2023-10-01 23:35:00 -0300

    testes efetuados

--- Secao03/area_de_testes.py
+++ Secao03/area_de_testes.py
@@ -1,2 +1,15 @@
+#coletamos o número
+num = int(input('Insira um número inteiro: '))
 
-
+# números inteiros iguais ou abaixo de 1 não consideramos primos
+if num > 1:
+    for i in range(2, num):
+        # verificamos todos os restos de divisões entre todos os números abaixo de num
+        # se algum resto for 0, então ele é divisível por outro número além dele e 1
+        if (num % i) == 0:
+            print(f'{num} não é um número primo')
+            break
+    else:
+        print(f'{num} é um número primo')
+else:
+    print(f'{num} não é um número primo')
--- aulas_extras/foor_loop.py
+++ aulas_extras/foor_loop.py
@@ -3,4 +3,4 @@ for magican in magicians:
     print(f'Congrats {magican.title()} that was a great trick!')
     print(f"I can't wait to see your next trick, {magican.title()} \n")
 
-    print("Thank you, everyone, that was a great magic show!!!!")
+    print("Thank you, everyone, that was a great magic show!!!! \n")
