d3722d5ab80b221d8e23f40baa1c1e8a2cd937f9
phvidal <vidal081089@gmail.com>
2023-10-01 23:35:34 -0300

    testes

--- aulas_extras/foor_loop.py
+++ aulas_extras/foor_loop.py
@@ -3,4 +3,5 @@ for magican in magicians:
     print(f'Congrats {magican.title()} that was a great trick!')
     print(f"I can't wait to see your next trick, {magican.title()} \n")
 
-    print("Thank you, everyone, that was a great magic show!!!! \n")
+print("Thank you, everyone, that was a great magic show!!!! \n")
+
--- aulas_extras/tempCodeRunnerFile.py
+++ aulas_extras/tempCodeRunnerFile.py
@@ -1,1 +1,5 @@
-pular
+squares = []
+for value in range(1,11):
+    square = value**2
+    squares.append(square)
+print(squares)
